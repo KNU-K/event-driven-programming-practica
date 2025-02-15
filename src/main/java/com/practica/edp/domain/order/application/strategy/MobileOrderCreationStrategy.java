d63d20238580242dece8c151752d9a668e843b49
KNU-K <knukang334@gmail.com>
2025-02-16 04:09:12 +0900

    feat: add MobileOrderCreationStrategy for mobile order creation management

---
+++ src/main/java/com/practica/edp/domain/order/application/strategy/MobileOrderCreationStrategy.java
@@ -0,0 +1,15 @@
+package com.practica.edp.domain.order.application.strategy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MobileOrderCreationStrategy implements OrderCreationStrategy{
+    private final ApplicationEventPublisher publisher;
+    @Override
+    public void createOrder() {
+
+    }
+}
